bff3d5d87302d91f90c35f6f6cdacf2011730741
alirfanyasin <risayunita21@gmail.com>
2024-01-15 21:31:40 +0700

    slicing splash page

---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,24 @@
+import 'package:bank_sha/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkBackgroundColor,
+      body: Center(
+        child: Container(
+          width: 155,
+          height: 50,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/logo_dark.png'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
