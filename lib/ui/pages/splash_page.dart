cac09234aa9e4abe6e325f624e600d93be5f77bb
alirfanyasin <risayunita21@gmail.com>
2024-01-16 11:47:37 +0700

    slicing onBoarding page part 1

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -1,10 +1,32 @@
+import 'dart:async';
+
 import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/pages/onboarding_page.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const OnBoardingPage(),
+        ),
+      );
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: darkBackgroundColor,
