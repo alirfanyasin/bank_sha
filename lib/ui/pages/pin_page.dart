06651b831e8776b9fe072b5859407a75e7ba469a
alirfanyasin <risayunita21@gmail.com>
2024-01-18 00:47:18 +0700

    navigation with result

--- lib/ui/pages/pin_page.dart
+++ lib/ui/pages/pin_page.dart
@@ -18,6 +18,10 @@ class _PinPageState extends State<PinPage> {
         pinController.text = pinController.text + number;
       });
     }
+
+    if (pinController.text == '123456') {
+      Navigator.pop(context, true);
+    }
   }
 
   deletePin() {
